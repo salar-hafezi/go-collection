198d98a477953bbb64ee08263645414dca98dda0
Salar Hafezi <salar.hfz@gmail.com>
2018-01-11 18:02:49 -0500

    add JSON for request add JSON type for response

--- 01/01.go
+++ 01/01.go
@@ -13,6 +13,9 @@ type helloResponse struct {
 	Date    string `json:",omitempty"`
 	ID      int    `json:"id, string"`
 }
+type helloRequest struct {
+	Name string `json:"name"`
+}
 
 func main() {
 	port := 8080
@@ -26,17 +29,34 @@ func main() {
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	logMessage := fmt.Sprintf("the req of type %s went to %s path", r.Method, r.URL)
 	log.Printf("Logger says:\n%s", logMessage)
+	/*
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Bad Request", http.StatusBadRequest)
+			return
+		}
+	*/
+	var reqObj helloRequest
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&reqObj)
+	if err != nil {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
 	response := helloResponse{
-		Message: "Thank you for the initiation :)",
+		Message: "Thank you for the initiation :). Hello " + reqObj.Name,
 		Author:  "Salar Hafezi",
 		Date:    "2018-01-11",
 		ID:      1371,
 	}
-	// data, err := json.MarshalIndent(response, "", "  ")
-	// if err != nil {
-	// 	fmt.Fprint(w, "Hello World\n")
-	// }
-	// fmt.Fprintf(w, string(data))
+	/*
+		data, err := json.MarshalIndent(response, "", "  ")
+		if err != nil {
+			fmt.Fprint(w, "Hello World\n")
+		}
+		fmt.Fprintf(w, string(data))
+	*/
 	encoder := json.NewEncoder(w)
 	encoder.Encode(response)
 }
