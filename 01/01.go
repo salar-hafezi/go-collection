c56134900bb88769a79e1e121ce8825ca9592494
Salar Hafezi <salar.hfz@gmail.com>
2018-01-11 17:41:28 -0500

    add encoder instead of marshal

--- 01/01.go
+++ 01/01.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type helloResponse struct {
+	Message string `json:"message"`
+	Author  string `json:"-"`
+	Date    string `json:",omitempty"`
+	ID      int    `json:"id, string"`
+}
 
 func main() {
 	port := 8080
@@ -18,5 +26,17 @@ func main() {
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	logMessage := fmt.Sprintf("the req of type %s went to %s path", r.Method, r.URL)
 	log.Printf("Logger says:\n%s", logMessage)
-	fmt.Fprint(w, "Hello World\n")
+	response := helloResponse{
+		Message: "Thank you for the initiation :)",
+		Author:  "Salar Hafezi",
+		Date:    "2018-01-11",
+		ID:      1371,
+	}
+	// data, err := json.MarshalIndent(response, "", "  ")
+	// if err != nil {
+	// 	fmt.Fprint(w, "Hello World\n")
+	// }
+	// fmt.Fprintf(w, string(data))
+	encoder := json.NewEncoder(w)
+	encoder.Encode(response)
 }
