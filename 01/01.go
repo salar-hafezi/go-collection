53c43cdb397067524c4f1709a23bddd823a433d2
Salar Hafezi <salar.hfz@gmail.com>
2018-01-11 17:12:15 -0500

    add logging to 01/01.go

--- 01/01.go
+++ 01/01.go
@@ -1,11 +1,12 @@
 package main
+
 import (
 	"fmt"
 	"log"
 	"net/http"
 )
 
-func main(){
+func main() {
 	port := 8080
 
 	http.HandleFunc("/hello", helloHandler)
@@ -14,6 +15,8 @@ func main(){
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
 }
 
-func helloHandler(w http.ResponseWriter, r *http.Request){
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	logMessage := fmt.Sprintf("the req of type %s went to %s path", r.Method, r.URL)
+	log.Printf("Logger says:\n%s", logMessage)
 	fmt.Fprint(w, "Hello World\n")
 }
