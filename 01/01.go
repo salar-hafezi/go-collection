8e5ba10ee23e398d9b9755e84b6d663073a2f474
Salar Hafezi <salar.hfz@gmail.com>
2018-01-11 17:04:16 -0500

    add 01.go

---
+++ 01/01.go
@@ -0,0 +1,19 @@
+package main
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main(){
+	port := 8080
+
+	http.HandleFunc("/hello", helloHandler)
+
+	log.Printf("Server starting on port %v\n", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request){
+	fmt.Fprint(w, "Hello World\n")
+}
